232b118ce72e27fd1a6e20d3d2f1d5f03659da40
LiubovKleimenova <kleimenovaliubov@gmail.com>
2019-09-18 21:41:18 -0400

    доисправлены ошибки eslint

--- js/stat.js
+++ js/stat.js
@@ -7,7 +7,7 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var CLOUD_COLOR = '#fff';
-var SHADOW_COLOR = "rgba(0, 0, 0, 0.7)";
+var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
 var GAP = 10;
 
 var CLOUD_PADDING = 20;
@@ -22,18 +22,18 @@ var COLUMN_GAP = 50;
 var GAP_COLUMN_NAME = 10;
 
 var namesY = CLOUD_Y + CLOUD_HEIGHT - CLOUD_PADDING;
-var columnY =  CLOUD_Y + CLOUD_HEIGHT - CLOUD_PADDING-LINE_HEIGHT;
+var columnY = CLOUD_Y + CLOUD_HEIGHT - CLOUD_PADDING - LINE_HEIGHT;
 
 
 // рисование прямоугольного облачка
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
-}
+};
 
 
 //  поиск максимального элемента в массиве
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
   for (var i = 1; i < arr.length; i++) {
@@ -45,7 +45,7 @@ var getMaxElement = function(arr) {
   return maxElement;
 };
 
-window.renderStatistics = function(ctx, players, times) {
+window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, (CLOUD_X + GAP), (CLOUD_Y + GAP), SHADOW_COLOR);
   renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
 
@@ -59,14 +59,14 @@ window.renderStatistics = function(ctx, players, times) {
 
   for (var i = 0; i < players.length; i++) {
     ctx.fillStyle = '#000';
-    ctx.fillText(players[i], CLOUD_X+CLOUD_PADDING+(COLUMN_WIDTH+COLUMN_GAP)*i, namesY);
+    ctx.fillText(players[i], CLOUD_X + CLOUD_PADDING + (COLUMN_WIDTH + COLUMN_GAP) * i, namesY);
 
     ctx.fillStyle = 'hsl(240,  ' + Math.random() * 100 + '%, ' + Math.random() * 100 + '%)';
     if (players[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     }
 
-    times[i]=Math.round(times[i]);
+    times[i] = Math.round(times[i]);
 
     var columnHeight = (MAX_COLUMN * times[i]) / maxTime;
 
